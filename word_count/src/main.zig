b089328e5b2fb18060d6983a9c6c90fe710afa74
DannyBimma <dantrotman92@gmail.com>
2025-08-02 12:09:32 -0400

    Include the stuff.

--- word_count/src/main.zig
+++ word_count/src/main.zig
@@ -1,1 +1,11 @@
+const std = @import("std");
+const printer = std.debug.print; // Cool lil' tick
 
+const TotalValues = struct {
+    chars: u32,
+    words: u32,
+    sentences: u32,
+    lines: u32,
+};
+
+pub fn main() !void {}
